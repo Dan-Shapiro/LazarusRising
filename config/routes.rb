6020eb585601dd09a43289c3ff689b601aef792f
Dan <ds5ct@virginia.edu>
2019-06-16 21:53:17 -0400

    custom page titles and descriptions, catch-all routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 	root 'home#index'
 
-	get 'our_team', to: 'our_team#index'
-	get 'press_room', to: 'press_room#index'
-	get 'get_involved', to: 'get_involved#index'
-	get 'form_submitted', to: 'form_submitted#index'
-	get 'privacy_policy', to: 'privacy_policy#index'
+	get 'our-team', to: 'our_team#index'
+	get 'press-room', to: 'press_room#index'
+	get 'get-involved', to: 'get_involved#index'
+	get 'form-submitted', to: 'form_submitted#index'
+	get 'privacy-policy', to: 'privacy_policy#index'
+
+	get '*path' => redirect('/')
 end
